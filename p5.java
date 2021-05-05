1f2f1bbba7828a81b17d9ed3c5944d019a07e398
bismi-tn <64467916+bism-tn@users.noreply.github.com>
2021-05-05 00:25:21 -0400

    Add files via upload

---
+++ p5.java
@@ -0,0 +1,47 @@
+public class p5 {
+    int rollno;
+    String name;
+    int mark1;
+    int mark2;
+    int mark3;
+
+    public p5(int no_get,String name_get,int m1,int m2,int m3){
+        this.rollno = no_get;
+        this.name = name_get;
+        this.mark1 = m1;
+        this.mark2 = m2;
+        this.mark3= m3;
+    }
+    public void disp(){
+        System.out.println("Roll No:"+this.rollno);
+        System.out.println("Name:"+this.name);
+        System.out.println("Mark1:"+this.mark1);
+        System.out.println("Mark2:"+this.mark2);
+        System.out.println("Mark3:"+this.mark3);
+		System.out.println("+++++++++++++++++++++");
+    }
+
+    public int total(){
+        int total  =  this.mark1 + this.mark2 + this.mark3;
+        return total;
+    }
+    public static void main(String[] args) {
+        p5 s1 =new p5(1,"Abc",10,29,38);
+        p5 s2 =new p5(2,"xyz",51,60,43);
+        p5 s3 =new p5(3,"pqr",41,42,43);
+		s1.disp();
+		s2.disp();
+		s3.disp();
+
+        if(s1.total() >= s2.total() && s1.total() >= s3.total()){
+            System.out.println("Highest score is for student : " + s1.name);
+        }
+        if(s2.total() >= s1.total() && s2.total() >= s3.total()){
+            System.out.println("Highest score is for student : " + s2.name);
+        }
+        if(s3.total() >= s2.total() && s3.total() >= s1.total()){
+            System.out.println("Highest score is for student : " + s3.name);
+        }
+
+    }
+}

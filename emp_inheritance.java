5aa6440c943d9c3a7b142ac29b1169e233930f51
bism-tn <bisminasseertk@gmail.com>
2021-08-11 10:31:29 +0530

    Added program10

---
+++ emp_inheritance.java
@@ -0,0 +1,71 @@
+import java.util.*;
+class Employee
+{
+	protected int empid,salary;
+	protected String name,address;
+	Scanner sc=new Scanner(System.in);
+	Employee(int id,String n,int sal,String ad)
+	{
+		this.empid=id;
+		this.name=n;
+		this.salary=sal;
+		this.address=ad;
+	}
+	void disp()
+	{
+		System.out.println("EMPLOYEE  ID  :"+empid);
+		System.out.println("EMPLOYEE NAME :"+name);
+		System.out.println("SALARY        :"+salary);
+		System.out.println("ADDRESS       :"+address);
+	}
+}
+class Teacher extends Employee
+{
+	String dpmt,sub;
+	Teacher(int id,String n,int sal,String ad,String d,String s)
+	{
+		super(id,n,sal,ad);
+		this.dpmt=d;
+		this.sub=s;
+	}
+	void disp()
+	{
+		super.disp();
+		System.out.println("DEPARTMENT    :"+dpmt);
+		System.out.println("SUBJECT       :"+sub);
+	}
+}
+class emp_inheritance
+{
+	public static void main(String[] args)
+	{
+		int n,empid,salary;
+		String name,address,dpmt,sub;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter Number of Teachers:");
+		n=Integer.parseInt(sc.nextLine());
+		Teacher t[]=new Teacher[n];
+		for(int i=0;i<n;i++)
+		{
+			System.out.println("\n--Enter details--\n");
+			System.out.print("Enter Employee ID:");
+			empid=Integer.parseInt(sc.nextLine());
+			System.out.print("Enter Employee Name:");
+			name=sc.nextLine();
+			System.out.print("Enter Salary:");
+			salary=Integer.parseInt(sc.nextLine());
+			System.out.print("Enter Address:");
+			address=sc.nextLine();
+			System.out.print("Enter Department:");
+			dpmt=sc.nextLine();
+			System.out.print("Enter Subject:");
+			sub=sc.nextLine();
+			t[i]=new Teacher(empid,name,salary,address,dpmt,sub);
+		}
+		for(int i=0;i<n;i++)
+		{
+			System.out.println("\n----------\nEMPLOYEE "+(i+1)+"\n---------- ");
+			t[i].disp();
+		}
+	}
+}
